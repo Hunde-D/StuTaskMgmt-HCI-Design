6d7de4702c3bcf80110d0d4d5536bc365a77dca1
Hunde-D <hunddh2025@gmail.com>
2024-05-26 20:33:14 +0300

    updt6

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,21 +9,19 @@ const Header = ({ className }) => {
     document.body.classList.toggle("dark");
   };
   return (
-    <div className="  h-12 w-full flex-auto items-center justify-center bg-light-Primary dark:bg-dark-Primary">
-      <div className=" flex h-full w-full justify-center bg-red-200">
-        <div className=" mr-12 flex h-4/6 rounded-md bg-gray-200 px-4 dark:bg-black">
-          <input
-            type="search"
-            placeholder="Search Anything.."
-            className=" mr-8 bg-transparent text-sm"
-          />
-        </div>
-        <div className="">
-          <button onClick={() => darkModeHandler()}>
-            {dark && <IoSunny />}
-            {!dark && <IoMoon />}
-          </button>
-        </div>
+    <div className="  flex h-12 w-full flex-auto items-center justify-end bg-light-Primary dark:bg-dark-Primary">
+      <div className=" flex h-4/6 rounded-md bg-gray-200 mr-52 px-4 dark:bg-black">
+        <input
+          type="search"
+          placeholder="Search Anything.."
+          className=" bg-transparent"
+        />
+      </div>
+      <div className="">
+        <button onClick={() => darkModeHandler()}>
+          {dark && <IoSunny className="text-white"/>}
+          {!dark && <IoMoon className="text-black"/>}
+        </button>
       </div>
     </div>
   );
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,11 +1,11 @@
-import { FaPlay } from "react-icons/fa";
+// import { FaPlay } from "react-icons/fa";
 const Task = () => {
   return (
     <div className="col-span-3 bg-light-Secondary dark:bg-dark-Secondary">
       <div className="flex w-full gap-3 bg-light-Primary dark:bg-dark-Primary">
         <div className="flex-auto bg-[#FBFAFF] p-3 dark:bg-[#212229]">
           <div className=" rounded-full bg-btn-primary">
-            <FaPlay className="text-white" />
+            {/* <FaPlay className="text-white" /> */}
           </div>
           <div>
             <p>Start here</p>
